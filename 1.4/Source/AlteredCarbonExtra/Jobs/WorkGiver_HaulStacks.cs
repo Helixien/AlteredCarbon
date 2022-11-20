d656c7d9b48f4b9d16cb7aabfa540f80de6cd1c0
Taranchuk <nfhfyxer@gmail.com>
2022-11-20 13:49:50 +0200

    update

--- 1.4/Source/AlteredCarbonExtra/Jobs/WorkGiver_HaulStacks.cs
+++ 1.4/Source/AlteredCarbonExtra/Jobs/WorkGiver_HaulStacks.cs
@@ -16,22 +16,19 @@ namespace AlteredCarbon
         public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
         {
             return CorticalStack.corticalStacks.Where(x => x.Spawned && pawn.Map == x.Map && x.PersonaData.ContainsInnerPersona 
-            && !pawn.Map.mapPawns.AllPawnsSpawned.Any(y => y.BillStack.Bills.Any(c => c is Bill_InstallStack installStack && installStack.stackToInstall == x))
+            && !pawn.Map.mapPawns.AllPawnsSpawned.Any(y => y.BillStack.Bills.Any(c => c is Bill_InstallStack installStack
+                && installStack.stackToInstall == x))
             && pawn.CanReserveAndReach(x, PathEndMode.Touch, Danger.Deadly));
-        }
-        public override bool ShouldSkip(Pawn pawn, bool forced = false)
-        {
-            return !CorticalStack.corticalStacks.Any(x => x.Spawned && pawn.Map == x.Map) || !Building_StackStorage.building_StackStorages
-                    .Any(x => x.HasFreeSpace && pawn.CanReserveAndReach(x, PathEndMode.Touch, Danger.Deadly));
         }
 
         public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
         {
-            return Building_StackStorage.building_StackStorages.Any(x => x.HasFreeSpace && x.Accepts(t) && pawn.CanReserveAndReach(x, PathEndMode.Touch, Danger.Deadly));
+            return Building_StackStorage.building_StackStorages.Any(x => x.HasFreeSpace && x.Accepts(t));
         }
         public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
         {
-            var containers = Building_StackStorage.building_StackStorages.Where(x => x.HasFreeSpace && x.Accepts(t) && pawn.CanReserveAndReach(x, PathEndMode.Touch, Danger.Deadly));
+            var containers = Building_StackStorage.building_StackStorages.Where(x => x.HasFreeSpace 
+                && x.Accepts(t) && pawn.CanReserveAndReach(x, PathEndMode.Touch, Danger.Deadly));
             if (containers.Any())
             {
                 var container = GenClosest.ClosestThing_Global_Reachable(pawn.Position, pawn.Map, containers, PathEndMode.Touch, TraverseParms.For(pawn));
