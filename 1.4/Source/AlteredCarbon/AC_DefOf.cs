1cadf7c05bf764c9badccb9a18812d2888b988af
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-02-04 18:38:43 +0100

    added AC_VoiceSynthesizer

--- 1.4/Source/AlteredCarbon/AC_DefOf.cs
+++ 1.4/Source/AlteredCarbon/AC_DefOf.cs
@@ -99,5 +99,7 @@ namespace AlteredCarbon
         public static SoundDef Message_NegativeEvent;
         public static XenotypeDef VFEU_Sleeveliner;
         public static ThingDef AC_GenomeRevitalizer, AC_NeutroamineDialyzer, AC_NeutroaminePump;
+
+        public static HediffDef AC_VoiceSynthesizer;
     }
 }
